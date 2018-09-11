d5f6fd4b53d55065cdd9dc2ec12c17c37f1bd2f1
Chris Morse <cjm@beeksma.com>
2018-09-11 12:10:28 -0700

    basic test example...  'bundle exec rspec spec'

---
+++ poppy/spec/hash/hash_spec.rb
@@ -0,0 +1,16 @@
+describe Hash do
+    before do
+        @grades = { "Jane Doe" => 10, "Jim Doe" => 6 }
+    end
+  
+    it "should return a blank instance" do
+        expect(Hash.new == {})
+    end
+  
+    it "hash the correct information in a key" do
+        expect(@grades["Jane Doe"]).to eq(10)
+        expect(@grades["Jim Doe"]).to eq(6)
+        expect(@grades["Not around"]).to eq(nil)
+    end
+
+  end
